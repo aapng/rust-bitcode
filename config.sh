227e11a48750c2078beeb31d512215618e7cf5b2
Anatoly Petrov <petrov.an@2Gis.ru>
2022-06-28 15:21:59 +0700

    Set Rust 1.55 Swift 5.5 LLVM

--- config.sh
+++ config.sh
@@ -5,18 +5,9 @@
 
 set -euxo pipefail
 
-RUST_TOOLCHAIN="1.60.0"
-
-# Note(Daniel): the official tagged releases of that llvm fork are currently
-# lagging too much behind the official `llvm`, which makes them incompatible
-# with the LLVM API required to compile the Rust toolchain, at least as of 1.60.
-# So neither swift-5.6.1-RELEASE nor swift-5.7-DEVELOPMENT-SNAPSHOT-2022-04-12-a
-#
-# But the tip of `apple/llvm-project`'s is too advanced as well, so I've had
-# to find a "sweet spot" in between both. I guess a decent approach could be
-# to start at https://github.com/rust-lang/rust/blob/1.60.0/.gitmodules#L37, and
-# to find a commit in `apple/llvm-project` close to it (ideally containing it)
-LLVM_BRANCH=bda51ce411586a8c012623300d8598ce84fced53
+RUST_TOOLCHAIN="1.55.0"
+SWIFT_VERSION="5.5"
+LLVM_BRANCH="swift/release/${SWIFT_VERSION}"
 
 get_rust_commit_for_toolchain() (
     # Yields "" for a toolchain like `x.y.z`, and `mm-dd-yy` for `nightly-mm-dd-yy`
@@ -35,4 +26,5 @@ RUST_BRANCH="$(get_rust_commit_for_toolchain)"
 
 # 3. Select a name for the toolchain you want to install as. The toolchain will be installed
 # under $HOME/.rustup/toolchains/rust-$RUST_TOOLCHAIN
-RUST_TOOLCHAIN="ios-arm64-${RUST_TOOLCHAIN}"
+RUST_TOOLCHAIN="ios-arm64-rust-${RUST_TOOLCHAIN}-swift-${SWIFT_VERSION}"
+
